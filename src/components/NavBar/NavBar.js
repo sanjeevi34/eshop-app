b84441291b0ff35574c241734538d3fa03ce53c0
sanjeevi34 <surehsanjeevi34@gmail.com>
2023-08-10 00:26:29 +0530

    Added searchbar in the NavBar after logging in

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,12 +2,19 @@ import { useState, useEffect } from "react";
 import Link from '@mui/material/Link';
 import Button from '@mui/material-next/Button';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
+import { Container, InputAdornment, TextField, makeStyles } from "@mui/material";
+
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+
 import { useHistory } from 'react-router-dom'; // Import useHistory
 import {
   AppBar,
   IconButton,
   Toolbar,
   Typography,
+  Grid
 } from "@mui/material";
 import styles from './NavBar.css';
 
@@ -52,9 +59,15 @@ const NavBar = (props) => {
         navigateTo('/');
     };
 
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+    };
+
     if (!props.loggedIn) {
         return (
-            <AppBar position="sticky">
+            <AppBar position="static">
                 <Toolbar>
                     <IconButton
                         size="large"
@@ -107,14 +120,37 @@ const NavBar = (props) => {
                     >
                         <ShoppingCartIcon />
                     </IconButton>
+
                     <Typography
                         variant="h6"
                         component="div"
                         align="left"
                         sx={{ flexGrow: 1}}
                     >
-                        Home
+                        upGrad E-Shop
                     </Typography>
+
+                    <TextField
+                      placeholder="Search..."
+                      variant="filled"
+                      sx={{ flex: 1, pr:30, pl:30}}
+                      InputProps={{
+                        startAdornment: (
+                          <InputAdornment position="start" sx={{ alignSelf: 'center' }}>
+                            <SearchIcon />
+                          </InputAdornment>
+                        ),
+                      }}
+                      onChange={handleChange}
+                    />
+
+                    <Link
+                      component="button"
+                      variant="h6"
+                      sx={{color: 'white', pr: 4}}
+                    >
+                      Home
+                    </Link>
 
                     <Link
                       component="button"
