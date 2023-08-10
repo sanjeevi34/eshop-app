dbd8ab43a411e358bbc69ef5ef951b05ada4ae58
sanjeevi34 <surehsanjeevi34@gmail.com>
2023-08-10 18:35:54 +0530

    If user not logged in, if try to access product page, redirects to login page

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -2,11 +2,20 @@ import NavBar from '../NavBar/NavBar.js';
 
 import { useAuth } from '../../Contexts/AuthContext';
 
+import { useHistory } from 'react-router-dom'; // Import useHistory
+
+
 const Products = () => {
     const {         authUser,
                     setAuthUser,
                     signedIn,
                     setSignedIn } = useAuth();
+
+    const history = useHistory(); // Get the history object
+
+    const navigateTo = (path) => {
+    history.push(path); // Use history.push to navigate to the specified path
+    };
 
     if(signedIn == true) {
         return (
@@ -17,12 +26,13 @@ const Products = () => {
         )
     }
     else {
-        return (
-            <div>
-            <NavBar loggedIn={false}/>
-            Not Signed In
-            </div>
-        )
+//        return (
+//            <div>
+//            <NavBar loggedIn={false}/>
+//            Not Signed In
+//            </div>
+//        )
+      navigateTo('/login');
     }
 }
 
